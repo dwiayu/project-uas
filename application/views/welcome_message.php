e58c05ccf177b78167ec5a1da0ac6736f7a97105
DESKTOP-482DCIA\dell <dwiayuningkinanti@gmail.com>
2018-06-25 13:09:20 -0500

    Folder CI

---
+++ application/views/welcome_message.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TUGAS CI</title>
+	<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css">
+<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap-theme.css">
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+  <img src="bird.jpg" alt="logo" style="width:40px;">
+  </a>> 
+</nav>
+
+<div class="container-fluid" align="center">
+  <h3>Hello World</h3><br><br><br>
+</div>
+<div class="container-fluid" align="center">
+<p><h2> Nama : Dwi Ayu Ning Kinanti</h2></p>
+<p><h2> NIM  : 1631710103</h2></p>
+<p><h2> Kelas: MI-2A</h2></p>
+</div>
+</body>
+</html>
