ca620c45abc020083b0f8ddc2bb3a19a834beefd
DESKTOP-LV1IMDI\ACER <indrimuktiw20@gmail.com>
2018-06-29 10:09:36 +0700

    update construct pada controller admin untuk multilevel

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,7 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
-
+public function __construct()
+    {
+        parent::__construct();
+        if($this->session->userdata('logged_in')){
+            $session_data=$this->session->userdata("logged_in");
+            $data['username']=$session_data['username'];
+            $data['level']=$session_data['level'];
+            $current_controller = $this->router->fetch_class();
+            $this->load->library('acl');
+            if(! $this->acl->is_public($current_controller)){
+                if(! $this->acl->is_allowed($current_controller, $data['level'])){
+                    redirect('login/logout','refresh');
+                }
+            }
+            $this->load->model('Admin_model');
+        }else{
+            redirect('login','refresh');
+        }
+    }
     
 
     public function index()
