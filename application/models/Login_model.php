1c70cb057f8949c56e4fe0b7e2ab391217df0fbb
DESKTOP-LV1IMDI\ACER <indrimuktiw20@gmail.com>
2018-06-29 10:20:10 +0700

    model untuk login multilevel

---
+++ application/models/Login_model.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login_model extends CI_Model{
+
+  public function __construct()
+  {
+    parent:: __construct();
+  }
+  public function login($username,$password)
+  {
+     $this->db->select('id,username,password,level');
+     $this->db->from('login');
+     $this->db->where('username', $username);
+     $this->db->where('password',MD5($password));
+     $query = $this->db->get();
+     if($query->num_rows()==1){
+         return $query->result();
+     }else{
+         return false;
+     }
+  }
+}
