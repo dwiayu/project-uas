618fbd57e450546c394406c7d7b3a4e38964ea52
DESKTOP-LV1IMDI\ACER <indrimuktiw20@gmail.com>
2018-06-29 10:05:07 +0700

    perubahan url dan database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/codeIgniter/';
+$config['base_url'] = 'http://localhost/project-uas/';
 
 /*
 |--------------------------------------------------------------------------
