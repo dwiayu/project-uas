8abb878925cbd1da72cc7975568f9e5a2746c4e9
DESKTOP-482DCIA\dell <dwiayuningkinanti@gmail.com>
2018-07-03 17:54:40 -0500

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/project-uas/index.php/depan';
+$config['base_url'] = 'http://localhost:81/project-uas/';
 
 /*
 |--------------------------------------------------------------------------
