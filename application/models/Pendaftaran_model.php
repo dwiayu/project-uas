6cc21c71a3573261fb1b538a5af0a0b3aaae6802
DESKTOP-482DCIA\dell <dwiayuningkinanti@gmail.com>
2018-07-03 17:32:13 -0500

    Application/models (model untuk pendaftaran)

---
+++ application/models/Pendaftaran_model.php
@@ -0,0 +1,26 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pendaftaran_model extends CI_Model{
+
+    public function __construct()
+    {
+        parent:: __construct();
+    }
+    
+    public function inputPendaftaran(){
+        $tanggal=$this->input->post('tanggal');
+        $tglraw=explode('-',$tanggal);
+        $tglfix=$tglraw[2].'-'.$tglraw[1].'-'.$tglraw[0];
+        $object = array('nama'=>$this->input->post('nama'),);
+        $object +=array('nim'=>$this->input->post('nim'),);
+        $object +=array('tempatLahir'=>$this->input->post('tempat'),);
+        $object +=array('tanggalLahir'=>$tglfix,);
+        $object +=array('jurusan'=>$this->input->post('jurusan'),);
+        $object +=array('alamat'=>$this->input->post('alamat'),);
+        $oject += array('noHp'=>$this->input->post('noHp'),);
+        $object+=array('prestasi'=>$this->input->post('prestasi'));
+        $this->db->insert('pendaftaran',$object);
+    }
+}
+?>
