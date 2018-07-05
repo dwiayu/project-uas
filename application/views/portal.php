fd7c705d2acf915d57d505a17ae295294dddad2d
DESKTOP-482DCIA\dell <dwiayuningkinanti@gmail.com>
2018-07-04 20:03:47 -0500

    update navbar pada portal view

--- application/views/portal.php
+++ application/views/portal.php
@@ -17,11 +17,11 @@
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="<?php echo site_url()?>/Portal">Home</a></li>
-      <li><a href="<?php echo site_url()?>/News">News</a></li>
-      <li><a href="<?php echo site_url()?>/Divisi">Divisi</a></li>
-      <li><a href="<?php echo site_url()?>/Prestasi">Prestasi</a></li>
-      <li><a href="<?php echo site_url()?>/Angkatan">Angkatan</a></li>
-      <li><a href="<?php echo site_url()?>/Admin">Admin</a></li>
+      <li><a href="<?php echo site_url()?>/news">News</a></li>
+      <li><a href="<?php echo site_url()?>/divisi">Divisi</a></li>
+      <li><a href="<?php echo site_url()?>/prestasi">Prestasi</a></li>
+      <li><a href="<?php echo site_url()?>/angkatan">Angkatan</a></li>
+      <li><a href="<?php echo site_url()?>/admin">Admin</a></li>
       <li><a href="<?php echo base_url('index.php/login/logout') ?>">Logout</a>
       
     </ul>
