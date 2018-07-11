424d0685b85ce8ddd937beec5d91d237acaa3a50
DESKTOP-LV1IMDI\ACER <indrimuktiw20@gmail.com>
2018-07-11 07:48:03 +0700

    update view untuk portal

--- application/views/portal.php
+++ application/views/portal.php
@@ -13,10 +13,10 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">DwiAyu</a>
+      <a class="navbar-brand" href="#">EEPROM</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="<?php echo site_url()?>/portal">Home</a></li>
+      <li class="active"><a href="<?php echo site_url()?>/Portal">Home</a></li>
       <li><a href="<?php echo site_url()?>/news">News</a></li>
       <li><a href="<?php echo site_url()?>/divisi">Divisi</a></li>
       <li><a href="<?php echo site_url()?>/prestasi">Prestasi</a></li>
